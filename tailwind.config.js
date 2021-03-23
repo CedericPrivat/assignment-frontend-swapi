3bdcb8029ee23ecaf1ba2ccff6e54a0f74cf1c50
Cederic Privat <cederic.privat@gmail.com>
2021-03-23 08:50:37 +0100

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './src/**/*.{html,ts}',
+    ]
+  },
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
