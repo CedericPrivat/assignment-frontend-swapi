c3ec55067de3275df5a8c2810e5ab04ebdce8715
Cederic Privat <cederic.privat@gmail.com>
2021-03-24 10:02:02 +0100

    Add loading indicator to routing events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import { Observable, merge } from 'rxjs';
+import { takeWhile, filter, mapTo } from 'rxjs/operators';
 
 @Component({
   selector: 'swapi-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  isRouting$!: Observable<boolean>;
+  protected alive = true;
+
+  constructor(private _router: Router) {
+    this._setupSplashScreen();
+  }
+
+  ngOnDestroy = () => this.alive = false;
+
+  private _setupSplashScreen() {
+    const navigationStart$ = this._router.events.pipe(
+      takeWhile(() => this.alive),
+      filter(event => event instanceof NavigationStart),
+      mapTo(true)
+    );
+
+    const navigationEnd$ = this._router.events.pipe(
+      takeWhile(() => this.alive),
+      filter(event => event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError),
+      mapTo(false)
+    );
+
+    this.isRouting$ = merge(navigationStart$, navigationEnd$);
+  }
+}
