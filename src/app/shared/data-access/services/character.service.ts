c556f91633f27ce017869fa1d94cb5597a988fae
Cederic Privat <cederic.privat@gmail.com>
2021-03-24 08:48:34 +0100

    Add getAll endpoint to character service

--- src/app/shared/data-access/services/character.service.ts
+++ src/app/shared/data-access/services/character.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { EMPTY, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Character } from '../models/character';
+import { HttpClient } from '@angular/common/http';
+import { ApiResponse } from '../responses/api-response';
+import { expand, reduce, shareReplay } from 'rxjs/operators';
+import { Helper } from '@shared/helper/utilities/helper';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterService {
+  characters$!: Observable<Character[]>;
 
-  constructor() { }
+  constructor(private _httpClient: HttpClient) { }
+
+  getAll(): Observable<Character[]> {
+    const url = `${environment.apiUrl}/people/`;
+    if (!this.characters$) {
+      this.characters$ = this._httpClient.get<ApiResponse>(url).pipe(
+        expand(res => res.next ? this._httpClient.get<ApiResponse>(Helper.changeUrlToHttps(res.next)) : EMPTY),
+        reduce((acc, res) => acc.concat(res.results), [] as Character[]),
+        shareReplay(1)
+      );
+    }
+    return this.characters$;
+  }
 }
