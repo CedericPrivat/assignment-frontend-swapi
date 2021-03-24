a1aa959b945f0de736acacd29fbff974c85d572f
Cederic Privat <cederic.privat@gmail.com>
2021-03-24 10:04:33 +0100

    Add getAll endpoint for film service

--- src/app/shared/data-access/services/film.service.ts
+++ src/app/shared/data-access/services/film.service.ts
@@ -1,15 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Helper } from '@shared/helper/utilities/helper';
+import { EMPTY, Observable } from 'rxjs';
+import { expand, reduce, shareReplay } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { Film } from '../models/film';
+import { ApiResponse } from '../responses/api-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmService {
+  films$!: Observable<Film[]>;
+
   constructor(private _httpClient: HttpClient) { }
 
-  getByUrl(url: string) {
+  getByUrl(url: string): Observable<Film> {
     return this._httpClient.get<Film>(Helper.changeUrlToHttps(url));
+  }
+
+  getAll(): Observable<Film[]> {
+    const url = `${environment.apiUrl}/films/`;
+    if (!this.films$) {
+      this.films$ = this._httpClient.get<ApiResponse>(url).pipe(
+        expand(res => res.next ? this._httpClient.get<ApiResponse>(Helper.changeUrlToHttps(res.next)) : EMPTY),
+        reduce((acc, res) => acc.concat(res.results), [] as Film[]),
+        shareReplay(1)
+      );
+    }
+    return this.films$;
   }
 }
