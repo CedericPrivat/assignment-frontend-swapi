49c1758595f755c69e4fab109831f1785469c2e2
Cederic Privat <cederic.privat@gmail.com>
2021-03-24 09:10:57 +0100

    Add characters markup to character list template

--- src/app/pages/characters/components/character-list/character-list.component.ts
+++ src/app/pages/characters/components/character-list/character-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Character } from '@shared/data-access/models/character';
+import { Helper } from '@shared/helper/utilities/helper';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -11,6 +12,7 @@ import { map } from 'rxjs/operators';
 })
 export class CharacterListComponent {
   characters$: Observable<Character[] | null>;
+  getIdFromUrl = Helper.getIdFromUrl;
 
   constructor(private _route: ActivatedRoute) {
     this.characters$ = this._createCharacters$();
